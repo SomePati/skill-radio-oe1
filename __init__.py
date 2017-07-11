597399c55004895ce0f20dc2d423ba213452b6d5
Fernandez Chris <cfernandez@protonmail.ch>
2017-07-10 20:33:40 -0700

    ok the actuall src code

---
+++ __init__.py
@@ -0,0 +1,58 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+# http://api.rtve.es/api/programas/1750/audios.rss
+# http://api.rtve.es/api/programas/36019/audios.rss
+
+import feedparser
+import time
+from os.path import dirname
+import re
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util import play_mp3
+from mycroft.util.log import getLogger
+
+
+__author__ = 'fernandez'
+
+LOGGER = getLogger(__name__)
+
+
+class RadioRncSkill(MycroftSkill):
+    def __init__(self):
+        super(RadioRneSkill, self).__init__(name="RadioRneSkill")
+        self.process = none
+
+    def initialize(self):
+        play_me_intent = IntentBuilder("RadioRneIntent"). \
+            require("RadioRneKeyword").build()
+        self.register_intent(play_me_intent)
+
+
+    def handle_play_me_intent(self, message):
+        try:
+            data = feedparser.parse("http://api.rtve.es/api/programas/36019/audios.rss")
+            self.speak_dialog('rne.news')
+            time.sleep(5)
+
+            self.process = play_mp3(
+                re.sub(
+                    'https', 'http', data['entries'][0]['links'][0]['href']))
+
+        except Exception as e:
+            LOGGER.error("Error: {0}".format(e))
+
+
+    def stop(self):
+        if self.process and self.process.poll() is None:
+            self.speak_dialog('rne.news.stop')
+            self.process.terminate()
+            self.process.wait()
+
+
+def create_skill():
+    return RadioRneSkill()
